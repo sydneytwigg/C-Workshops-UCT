892848c26fef8dc86b5020f4bf1a6e31832fb0ec
Sydney <sydneytwigg@gmail.com>
2019-02-14 21:35:39 +0200

    Fixed buttons

--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -17,6 +17,16 @@ namespace PictureViewer
             InitializeComponent();
         }
 
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Show the Open File dialog. If the user clicks OK, load the
+            // picture that the user chose
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
         private void clearButton_Click(object sender, EventArgs e)
         {
             // Clear the picture.
