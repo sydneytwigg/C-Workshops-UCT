128f0d908e8dfce667459d6f881cf30a7ef3fce8
Sydney Twigg <TWGSYD001@myuct.ac.za>
2019-02-19 08:00:11 +0200

    MVC1 project files

--- ContosoUniversity/ContosoUniversity/Properties/AssemblyInfo.cs
+++ ContosoUniversity/ContosoUniversity/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b6a0b6e-17ca-40f8-9ed2-20d107cfebf0")]
+[assembly: Guid("23df2c36-63b0-415b-a4b0-83af201f5559")]
 
 // Version information for an assembly consists of the following four values:
 //
