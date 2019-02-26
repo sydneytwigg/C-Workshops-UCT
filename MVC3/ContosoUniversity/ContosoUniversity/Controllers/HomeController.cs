08535ada2632f038ecd43c7b3139930aea30e329
Sydney Twigg <TWGSYD001@myuct.ac.za>
2019-02-26 08:04:20 +0200

    MVC3 added, migrations not working correctly

---
+++ MVC3/ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //IQueryable<EnrollmentDateGroup> data = from student in db.Students
+            //                                       group student by student.EnrollmentDate into dateGroup
+            //                                       select new EnrollmentDateGroup()
+            //                                       {
+            //                                           EnrollmentDate = dateGroup.Key,
+            //                                           StudentCount = dateGroup.Count()
+            //                                       };
+            IQueryable<CourseTitleGroup> data = from enrolledCourse in db.Enrollments
+                                                group enrolledCourse by enrolledCourse.CourseID into courseGroup
+                                                orderby courseGroup.Count() descending
+                                                select new CourseTitleGroup()
+                                                {
+                                                    Title = (from course in db.Courses where course.CourseID == courseGroup.Key select course.Title).FirstOrDefault(),
+                                                    StudentCount = courseGroup.Count()
+                                                };
+            return View(data.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
